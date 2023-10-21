77fcc13aab3bb06b379c8fa409edfaf8dcea684a
AnYun <coding@anyun.dev>
2023-10-21 15:43:30 +0800

    Add Whisper Code

--- WhisperSample/Pages/Index.cshtml.cs
+++ WhisperSample/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Drawing;
 
 namespace WhisperSample.Pages
 {
@@ -12,9 +15,31 @@ namespace WhisperSample.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnPost(IFormFile voicefile)
         {
+            var apikey = "{your api key}";
+            var apiUrl = "https://{your openai endpoint}.openai.azure.com";
 
+            var client = new OpenAIClient(new Uri(apiUrl), new AzureKeyCredential(apikey));
+            try
+            {
+                var transcriptionOptions = new AudioTranscriptionOptions()
+                {
+                    AudioData = BinaryData.FromStream(voicefile.OpenReadStream()),
+                    ResponseFormat = AudioTranscriptionFormat.Verbose,
+                };
+
+                Response<AudioTranscription> transcriptionResponse = await client.GetAudioTranscriptionAsync(
+                    deploymentId: "{your deploy}",
+                    transcriptionOptions);
+                AudioTranscription transcription = transcriptionResponse.Value;
+
+                return Content(transcription.Text);
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
         }
     }
 }
